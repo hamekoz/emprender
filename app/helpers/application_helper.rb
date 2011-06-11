8f1717d819debe5ec2d1d5a5b28d95e987ac592f
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2011-06-11 19:06:49 -0300

    Agregados Helpers para manejo de boton Difinidos estilos de colores Definidos estilos de botones Definicion de bordes redondeados Limpieza de archivos no necesarios Modificacion de elaces de acciones para usar los helpers de botones

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def boton_mostrar(accion)
+    return link_to 'Mostrar', accion, :class => 'mostrar boton naranja'
+  end
+
+  def boton_agregar(accion)
+    return link_to 'Agregar', accion, :class => 'agregar boton verde'
+  end
+
+  def boton_editar(accion)
+    return link_to 'Editar', accion, :class => 'editar boton coral'
+  end
+
+  def boton_eliminar(accion)
+    return link_to 'Eliminar', accion, :confirm => '¿Esta seguro?', :method => :delete, :class => 'eliminar boton rojo'
+  end
+
+  def boton_volver(accion)
+    return link_to 'Volver', accion, :class => 'volver boton gris'
+  end
+
 end
---
+++ test/unit/helpers/postulantes_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PostulantesHelperTest < ActionView::TestCase
+end
