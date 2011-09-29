8ff76bf8a93ee603aecb25e51b0b663588b3c14f
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2011-09-29 14:51:07 -0300

    Corregido problema por falta de validacion

--- app/models/documento.rb
+++ app/models/documento.rb
@@ -1,4 +1,4 @@
 class Documento < ActiveRecord::Base
   validates :nombre, :uniqueness => true
-            :abreviatura :presence =>true, :uniqueness => true
+  validates :abreviatura :presence =>true, :uniqueness => true
 end
