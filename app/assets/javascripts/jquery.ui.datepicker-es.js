91a0b7e97051054eff583bb6bcc22eba8311a1fe
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2012-04-14 22:39:17 -0300

    Limpieza de tildes

--- app/assets/javascripts/jquery.ui.datepicker-es.js
+++ app/assets/javascripts/jquery.ui.datepicker-es.js
@@ -12,8 +12,8 @@ jQuery(function($){
 		'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
 /*		monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun',
 		'Jul','Ago','Sep','Oct','Nov','Dic'],*/
-		dayNames: ['Domingo','Lunes','Martes','Mi&eacute;rcoles','Jueves','Viernes','S&aacute;bado'],
-		dayNamesShort: ['Dom','Lun','Mar','Mi&eacute;','Juv','Vie','S&aacute;b'],
+		dayNames: ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'],
+		dayNamesShort: ['Dom','Lun','Mar','Mie;','Juv','Vie','S&aacute;b'],
 		dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','S&aacute;'],
 		weekHeader: 'Sm',
 		dateFormat: 'dd/mm/yy',
