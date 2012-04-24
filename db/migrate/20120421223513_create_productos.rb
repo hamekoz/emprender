5b8b710f51d00cd949b0860107cbde13d8080067
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2012-04-24 19:13:45 -0300

    Merge origin

--- db/migrate/20120421223513_create_productos.rb
+++ db/migrate/20120421223513_create_productos.rb
@@ -2,14 +2,14 @@ class CreateProductos < ActiveRecord::Migration
   def change
     create_table :productos do |t|
       t.references :rubro
-	  t.string :nombre
+    t.string :nombre
       t.string :descripcion
       t.string :imagen1
       t.string :imagen2
       t.string :imagen3
       t.string :imagen4
       t.string :imagen5
-	  t.boolean :activo
+    t.boolean :activo
 
       t.timestamps
     end
