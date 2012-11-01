6fc389f6bd3d61ee72ca68b6b4b387510aa174fc
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2012-10-31 22:31:54 -0300

    Correcciones de traduccion Agregadas acciones bloquear y desbloquear para rails_admin

--- lib/rails_admin_acciones_emprender.rb
+++ lib/rails_admin_acciones_emprender.rb
@@ -19,7 +19,7 @@ module RailsAdmin
             @objects.each do |object|
               object.aceptar
             end
-            flash[:success] = "#{@model_config.label} aceptado."
+            flash[:success] = "#{@objects.count} #{@model_config.label_plural} aceptados"
             redirect_to back_or_index
           end
         end
@@ -36,7 +36,7 @@ module RailsAdmin
             @objects.each do |object|
               object.rechazar
             end
-            flash[:success] = "#{@model_config.label} rechazado."
+            flash[:success] = "#{@objects.count} #{@model_config.label_plural} rechazado"
             redirect_to back_or_index
           end
         end
@@ -53,7 +53,7 @@ module RailsAdmin
             @objects.each do |object|
               object.activar
             end
-            flash[:success] = "#{@model_config.label} activado."
+            flash[:success] = "#{@objects.count} #{@model_config.label_plural} activado"
             redirect_to back_or_index
           end
         end
@@ -70,7 +70,7 @@ module RailsAdmin
             @objects.each do |object|
               object.desactivar
             end
-            flash[:success] = "#{@model_config.label} desactivado."
+            flash[:success] = "#{@objects.count} #{@model_config.label_plural} desactivados"
             redirect_to back_or_index
           end
         end
@@ -87,7 +87,7 @@ module RailsAdmin
             @objects.each do |object|
               object.publicar
             end
-            flash[:success] = "#{@model_config.label} Publicado."
+            flash[:success] = "#{@objects.count} #{@model_config.label_plural} publicados"
             redirect_to back_or_index
           end
         end
@@ -104,7 +104,41 @@ module RailsAdmin
             @objects.each do |object|
               object.despublicar
             end
-            flash[:success] = "#{@model_config.label} despublicado."
+            flash[:success] = "#{@objects.count} #{@model_config.label_plural} despublicados"
+            redirect_to back_or_index
+          end
+        end
+      end
+
+      class Bloquear < RailsAdmin::Config::Actions::Base
+        RailsAdmin::Config::Actions.register(self)
+        register_instance_option :bulkable? do
+          true
+        end
+        register_instance_option :controller do
+          Proc.new do
+            @objects = list_entries(@model_config, :destroy)
+            @objects.each do |object|
+              object.bloquear
+            end
+            flash[:success] = "#{@objects.count} #{@model_config.label_plural} bloqueados"
+            redirect_to back_or_index
+          end
+        end
+      end
+
+      class Desbloquear < RailsAdmin::Config::Actions::Base
+        RailsAdmin::Config::Actions.register(self)
+        register_instance_option :bulkable? do
+          true
+        end
+        register_instance_option :controller do
+          Proc.new do
+            @objects = list_entries(@model_config, :destroy)
+            @objects.each do |object|
+              object.desbloquear
+            end
+            flash[:success] = "#{@objects.count} #{@model_config.label_plural} desbloqueados"
             redirect_to back_or_index
           end
         end
